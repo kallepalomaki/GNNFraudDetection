ba7c510076b7c91b1235ebb4364bf3bafaf10ce2
Kalle Palomäki <kpalomak@Kalles-MacBook-Pro.local>
2025-03-05 22:54:56 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+import pandas as pd
+import torch
+device = torch.device('cpu')
+import dgl
+#df=pd.read_csv("../data/PaySim_kaggle.csv")
+
+#df=df.sample(n=500)
+
+#print(df.head())
